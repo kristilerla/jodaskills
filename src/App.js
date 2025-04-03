38b6fecd9a10787c4d503a1a4763b420cc2492f2
Kristil Erla Håland <78584348+kristilerla@users.noreply.github.com>
2025-04-03 21:58:12 +0200

    Rydder vekk node_modules og unødvendige filer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ function App() {
   return (
     <div className="container">
       <CourseViewer file="ernaering_clean.md" />
+      
+      <div className="bg-blue-200 text-blue-900 p-4 rounded-lg">
+    Hvis du ser blå bakgrunn og blå tekst – Tailwind virker! 🎉
+  </div>
     </div>
   );
 }
